1fdb6279c46a2ba3608ab0fe0c40c907ea3f1622
Nami-Rangana <t.desilva@ufl.edu>
2024-12-13 16:35:00 -0500

    update codes

--- compress/compress.py
+++ compress/compress.py
@@ -2,12 +2,26 @@ import utils
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import datasets, transforms
+import pytorch_lightning as pl
 
-# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+class RMSELoss(nn.Module):
+    def __init__(self):
+        super(RMSELoss, self).__init__()
+
+    def forward(self, recon, x):
+        rmse = torch.sqrt(torch.mean((recon - x) ** 2))
+        return rmse
+
 
 class AE(nn.Module):
-    def __init__(self, n_atoms, n_channels=4096, latent_dim=20):
+    def __init__(self, n_atoms:int, latent_dim:int=20, n_channels:int=4096):
+        r'''
+pytorch-Lightning AutoEncoder
+-----------------------------
+n_atoms (int) : total number of atoms in a single trajectory frame
+latent_dim (int) : compressed latent vector length [Default=20]
+        '''
         super().__init__()
         
         self.n_channels = n_channels
@@ -66,10 +80,61 @@ class AE(nn.Module):
         decoded = self.decoder(encoded)
         return decoded
 
-class RMSDLoss(nn.Module):
-    def __init__(self):
-        super(RMSDLoss, self).__init__()
 
-    def forward(self, recon, x):
-        rmsd = torch.sqrt(torch.mean((recon - x) ** 2))
-        return rmsd
+class LightAE(pl.LightningModule):
+    def __init__(self, model, lr=1e-4, weight_decay=0, idx=None):
+        r'''
+pytorch-Lightning AutoEncoder
+-----------------------------
+model : pytorch model
+lr (float, Tensor, optional) : learning rate [default: 1e-4]
+weight_decay (float, optional) : weight decay (L2 penalty) [default=0]
+idx : a list of indices or a list of tuples of indices that specify a subset of the data x and recon to be used in the loss calculation. [Default=None]
+When idx is not None, it is assumed to be a list of tuples, where each tuple contains two indices i[0] and i[1]. These indices are used to slice the data x and recon along the first axis (i.e., x[:, i[0]:i[1]] and recon[:, i[0]:i[1]]). The loss is then calculated for each slice separately, and the results are summed up.
+        '''
+        super().__init__()
+        self.model = model
+        self.loss_fn = RMSELoss()
+        self.optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+        self.idx = idx
+        self.training_losses = []
+        self.epoch_losses = []
+
+    def forward(self, x):
+        return self.model(x)
+
+    def _calculate_loss(self, x, recon, idx):
+        if idx is None:
+            return self.loss_fn(recon, x)
+        else:
+            return sum(self.loss_fn(x[:, i[0]:i[1]], recon[:, i[0]:i[1]]) for i in idx)
+
+    def training_step(self, batch, batch_idx):
+        x = batch.to(self.device)
+        recon = self.model(x)
+        loss = self._calculate_loss(x, recon, self.idx)
+        self.log('train_loss', loss, on_epoch=True)
+        self.training_losses.append(loss.detach().cpu().item())
+        return {'loss': loss}
+
+    # def on_train_epoch_end(self):
+    #     avg_train_loss = torch.tensor(self.training_losses).mean()
+    #     self.log('avg_train_loss', avg_train_loss, prog_bar=True, logger=True)
+    #     self.training_losses.clear()
+    def on_train_epoch_end(self):
+        epoch_loss = torch.tensor(self.training_losses).mean()
+        self.log('Epoch Loss', epoch_loss, prog_bar=True, logger=False)
+        self.epoch_losses.append(epoch_loss.detach().cpu().item())
+        self.training_losses.clear()
+
+    def on_train_end(self):
+        print('Autoencoder training complete\n')
+        print('_'*70,'\n')
+        with open("losses.dat", "w") as f:
+            for i, loss in enumerate(self.epoch_losses):
+                f.write(f'{i:4d}\t {loss:8.5f}\n')
+
+    def configure_optimizers(self):
+        return self.optimizer
+
+
--- compress/utils.py
+++ compress/utils.py
@@ -1,19 +1,66 @@
-import numpy as np
 import mdtraj as md
+import numpy as np
+import os
+from tqdm import tqdm
+import time
 
-def read_traj(xtc_:str, top_:str, ref_:str=None):
-    traj = md.load(xtc_, top=top_)
-    
-    # Remove rotation: algn on 1st frame of the trajectory or a given reference pdb frame
-    traj.superpose(traj, 0 if ref_ == None else ref_) 
-    
-    # Remove translation: Center the COM of each frame
-    for i in range(traj.n_frames):
-        frame_positions = traj.xyz[i, :, :]
-        com = np.mean(frame_positions, axis=0)
-        traj.xyz[i, :, :] -= com
-    
-    return traj.xyz, traj.n_atoms
+# import sys
+from torch.utils.data import DataLoader
 
-def minMaxScale(array:np.ndarray):
-    return (array - array.min()) / (array.max() - array.min())
+def read_traj_to_dl(traj_:str, top_:str, memmap:bool=False, stride:int=1, chunk:int=1000, batch_size:int=128):
+    r"""
+Create a Dataloader to train compressor model.
+----------------------------------------------
+traj_ (str) : Trajectory path -- netcdf format
+top_ (str) : Topology file 
+memmap (bool) : Create memory map (if RAM is not enough) [Default=False]
+stride (int) : Only read every stride-th frame [Default=1]
+chunk (int) : Number of frames to load at once from disk per iteration. If 0, load all [Default=1000]
+batch_size (int) : samples per batch to load [Default=128]
+    """
+    if chunk < stride:
+        raise ValueError('chunk should be higher than stride')
+        
+    with md.open(traj_, 'r') as t:  # Open in read-only mode
+        n_frames = t.n_frames
+        n_atoms = t.n_atoms
+
+    print(f'\nTrajectory stats : #_Frames = {n_frames}\t#_Atoms = {n_atoms}')
+    print('_'*70,'\n')
+    print(f'Start reading coordinates from trajectory to train model...\n[{int(n_frames/stride)} frames with stride {stride}]')
+
+    if memmap:
+        centered_xyz = np.memmap('temp_traj.dat', dtype=np.float32, mode='w+', shape=(int(n_frames/stride), n_atoms, 3))
+    else:
+        centered_xyz = np.empty(dtype=np.float32, shape=(int(n_frames/stride), n_atoms, 3))
+      
+    start_frame = 0
+    for n, chunk in tqdm(enumerate(md.iterload(traj_, top=top_, chunk=chunk, stride=stride)), total=int(n_frames/(chunk*stride)), bar_format='Loading trajectory: {percentage:6.2f}% |{bar}|', ncols=50):
+
+        # Remove rotation: algn on 1st frame of the trajectory or a given reference pdb frame
+        if n == 0:
+            init_frame = chunk[0]
+    
+        md.Trajectory.superpose(chunk, init_frame)
+
+        # Remove translation: Center the COM of each frame
+        for i in range(chunk.n_frames):
+            frame_positions = chunk.xyz[i, :, :]
+            com = np.mean(frame_positions, axis=0)
+            centered_xyz[start_frame + i, :, :] = frame_positions - com
+    
+        start_frame += chunk.n_frames
+    if memmap:
+        centered_xyz.flush()
+        
+
+    traj_dl = DataLoader(centered_xyz.reshape(-1,1,n_atoms,3), batch_size=batch_size, shuffle=True, drop_last=True)
+    print('\nDataLoader created')
+    print('_'*70,'\n')
+    
+    del centered_xyz
+
+    if memmap:
+        os.remove('temp_traj.dat')
+
+    return traj_dl, n_atoms
