c62c1b2bc598ea3a0a3d9464d29edc51d1eea084
nami-rangana <nami.rangana@gmail.com>
2025-01-31 20:57:33 -0500

    update

--- molzip/molzip.py
+++ molzip/molzip.py
@@ -19,10 +19,10 @@ from .autoencoder import *
 set_seed()
 
 # -------------------------------------------- 
-#             C O M P R E S S                  
+#                  T R A I N                  
 # -------------------------------------------- 
 
-def compress(traj:str, top:str, stride:int=1, out:str=os.getcwd(), fname:str='', epochs:int=100, batchSize:int=128, lat:int=20, w:float=1.0, memmap:bool=False):
+def train(traj:str, top:str, stride:int=1, out:str=os.getcwd(), fname:str='', epochs:int=100, batchSize:int=128, lat:int=20, w:float=1.0, memmap:bool=False):
     r'''
 compressing trajectory
 ----------------------
@@ -70,7 +70,7 @@ memmap (bool) : Use memory-map to read trajectory [Default=False]
     else:
         device = torch.device('cpu')
         accelerator = 'cpu'
-        n_devices = None
+        n_devices = 1
         print('CUDA is not available')
 
     # Read trajectory -------
@@ -105,7 +105,187 @@ memmap (bool) : Use memory-map to read trajectory [Default=False]
     
     torch.save(model, out+fname+'model.pt')
 
-    # Compress --------------
+    # # Compress --------------
+    # encoder = model.model.encoder.to(device)
+    # traj_dl = DataLoader(traj_, batch_size=batchSize, shuffle=False, drop_last=False, num_workers=4) 
+
+    # z = []
+    # with torch.no_grad():
+    #     encoder.eval()
+    #     for batch in tqdm(traj_dl, desc="Compressing "):
+    #         batch = batch.to(device=device, dtype=torch.float32)
+    #         z.append(encoder(batch))
+    
+    # pickle.dump(z, open(out+fname+"compressed.pkl", 'wb'))
+    # print('_'*70+'\n')
+
+    # # Print stats -----------
+    # org_size = os.path.getsize(traj)
+    # comp_size = os.path.getsize(out+fname+"compressed.pkl")
+    # compression = 100*(1 - comp_size/org_size)
+    
+    # template = "{string:<20} :{value:15.3f}"
+    # print(template.format(string='Original Size [MB]', value=round(org_size*1e-6,3)))
+    # print(template.format(string='Compressed Size [MB]', value=round(comp_size*1e-6,3)))
+    # print(template.format(string='Compression %', value=round(compression,3)))
+    # print('---')
+    # # print(template.format(string='RMSD [\u212B]', value=round(np.mean(rmsd),3)))
+    # # print(template.format(string='R\u00b2', value=round(r2,3))) 
+    # # print(template.format(string='MSE (nm\u00b2)', value=round(mean_squared_error,3)))
+    
+    # Clean -----------------
+    if os.path.exists('lightning_logs'):
+        shutil.rmtree('lightning_logs')
+    if os.path.exists('temp_traj.dat'):
+        os.remove('temp_traj.dat')
+
+    print('\n')
+ 
+# -------------------------------------------- 
+#        C O N T I N U E  T R A I N                  
+# -------------------------------------------- 
+
+def cont_train(traj:str, top:str,  model:str, checkpoint:str, stride:int=1, epochs:int=100, batchSize:int=128, lat:int=20, w:float=1.0, memmap:bool=False):
+    r'''
+compressing trajectory
+----------------------
+traj (str) : Path to the trajectory file
+top (str) : Path to the topology file
+stride (int) : Read every strid-th frame [Default=1]
+model (str)  = Path to previously trained model file
+checkpoint (str) = Path to previously check point file
+out (str) : Path to save compressed files [Default=current directory]
+fname (str) : Prefix for all generated files [Default=None]
+epochs (int) : Number of epochs to train AE model [Default=100]
+batchSize (int) : Batch size to train AE model [Default=128]
+lat (int) : Latent vector length [Default=20]
+w (float) : Non-negative weight for loss function [Default=1.0]
+memmap (bool) : Use memory-map to read trajectory [Default=False]
+        '''
+    
+    pathExists(traj)
+    pathExists(top)
+
+    # Define device ---------
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        accelerator = 'gpu'
+        n_devices = torch.cuda.device_count()
+        if n_devices == 1:
+            print('Device name:', torch.cuda.get_device_name(device))
+        else:
+            print(f'Available devices: {n_devices:>02d}')
+            for i in range(n_devices):
+                print(torch.cuda.get_device_name(i))
+    else:
+        device = torch.device('cpu')
+        accelerator = 'cpu'
+        n_devices = None
+        print('CUDA is not available')
+
+    # Load model
+    model = torch.load(model).to(device)
+    
+    out = os.path.dirname(model.loss_path)
+    fname = os.path.basename(model.loss_path).split('_')[0] + '_'
+    
+    
+    if platform.system() == "Windows":
+        if not out.endswith('\\'):
+            out += '\\'
+    else:
+        if not out.endswith('/'):
+            out += '/'
+
+    # if os.path.exists(out+fname+'compressed'):
+    #     shutil.rmtree(out+fname+'compressed')
+    # os.mkdir(out+fname+'compressed')
+    # out = out+fname+'compressed\\' if platform.system() == "Windows" else out+fname+'compressed/'
+
+    # Read trajectory -------
+    traj_ = read_traj(traj_=traj, top_=top, stride=stride, memmap=memmap)
+    traj_dl = DataLoader(traj_, batch_size=batchSize, shuffle=True, drop_last=True, num_workers=4)
+    print('_'*70+'\n')
+
+    # Train model -----------
+    model.epoch_losses = list(np.loadtxt(model.loss_path, usecols=1))
+    
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=out,
+        filename=f'{fname}checkpoint',
+        save_top_k=1  # Save the best checkpoint
+        )
+    
+    trainer = pl.Trainer(
+        max_epochs=len(model.epoch_losses)+epochs,
+        accelerator=accelerator,
+        devices=n_devices,
+        callbacks=[checkpoint_callback]
+        )
+    
+    trainer.fit(model, traj_dl, ckpt_path=checkpoint)
+    
+    # Clean -----------------
+    if os.path.exists('lightning_logs'):
+        shutil.rmtree('lightning_logs')
+    if os.path.exists('temp_traj.dat'):
+        os.remove('temp_traj.dat')
+
+    print('\n')
+
+
+# -------------------------------------------- 
+#             C O M P R E S S                  
+# -------------------------------------------- 
+def compress(traj:str, top:str, model:str, stride:int=1, out:str=os.getcwd(), fname:str=None, batchSize:int=128, memmap:bool=False):
+    r'''
+compressing trajectory
+----------------------
+traj (str) : Path to the trajectory file
+top (str) : Path to the topology file
+model (str)  = Path to previously trained model file
+stride (int) : Read every strid-th frame [Default=1]
+out (str) : Path to save compressed files [Default=current directory]
+fname (str) : Prefix for all generated files [Default=None]
+batchSize (int) : Batch size to train AE model [Default=128]
+memmap (bool) : Use memory-map to read trajectory [Default=False]
+        '''
+    pathExists(traj)
+    pathExists(top)
+    # Define device ---------
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        n_devices = torch.cuda.device_count()
+        if n_devices == 1:
+            print('Device name:', torch.cuda.get_device_name(device))
+        else:
+            print(f'Available devices: {n_devices:>02d}')
+            for i in range(n_devices):
+                print(torch.cuda.get_device_name(i))
+    else:
+        device = torch.device('cpu')
+        n_devices = 1
+        print('CUDA is not available')
+    
+    model = torch.load(model).to(device)
+    
+    if fname != None:
+        fname += '_'
+    else:
+        fname = os.path.basename(model.loss_path).split('_')[0] + '_'
+    
+    if platform.system() == "Windows":
+        if not out.endswith('\\'):
+            out += '\\'
+    else:
+        if not out.endswith('/'):
+            out += '/'
+
+    # Read trajectory -------
+    traj_ = read_traj(traj_=traj, top_=top, stride=stride, memmap=memmap)
+    traj_dl = DataLoader(traj_, batch_size=batchSize, shuffle=True, drop_last=True, num_workers=4)
+    print('_'*70+'\n')
+    
     encoder = model.model.encoder.to(device)
     traj_dl = DataLoader(traj_, batch_size=batchSize, shuffle=False, drop_last=False, num_workers=4) 
 
@@ -129,102 +309,7 @@ memmap (bool) : Use memory-map to read trajectory [Default=False]
     print(template.format(string='Compressed Size [MB]', value=round(comp_size*1e-6,3)))
     print(template.format(string='Compression %', value=round(compression,3)))
     print('---')
-    # print(template.format(string='RMSD [\u212B]', value=round(np.mean(rmsd),3)))
-    # print(template.format(string='R\u00b2', value=round(r2,3))) 
-    # print(template.format(string='MSE (nm\u00b2)', value=round(mean_squared_error,3)))
     
-    # Clean -----------------
-    if os.path.exists('lightning_logs'):
-        shutil.rmtree('lightning_logs')
-    if os.path.exists('temp_traj.dat'):
-        os.remove('temp_traj.dat')
-
-    print('\n')
-    
-# -------------------------------------------- 
-#      C O N T I N U E  C O M P R E S S                  
-# -------------------------------------------- 
-
-def cont_compress(traj:str, top:str,  model:str, checkpoint:str, stride:int=1, epochs:int=100, batchSize:int=128, lat:int=20, w:float=1.0, memmap:bool=False):
-    r'''
-compressing trajectory
-----------------------
-traj (str) : Path to the trajectory file
-top (str) : Path to the topology file
-stride (int) : Read every strid-th frame [Default=1]
-model (str)  = Path to previously trained model file
-checkpoint (str) = Path to previously check point file
-out (str) : Path to save compressed files [Default=current directory]
-fname (str) : Prefix for all generated files [Default=None]
-epochs (int) : Number of epochs to train AE model [Default=100]
-batchSize (int) : Batch size to train AE model [Default=128]
-lat (int) : Latent vector length [Default=20]
-w (float) : Non-negative weight for loss function [Default=1.0]
-memmap (bool) : Use memory-map to read trajectory [Default=False]
-        '''
-    
-    pathExists(traj)
-    pathExists(top)
-
-    # Load model
-    model = torch.load(model).to(device)
-    
-    out = os.path.dirname(model.loss_path)
-    fname = os.path.basename(model.loss_path).split('_')[0]
-    
-    if len(fname) != 0:
-        fname += '_'
-    
-    if platform.system() == "Windows":
-        if not out.endswith('\\'):
-            out += '\\'
-    else:
-        if not out.endswith('/'):
-            out += '/'
-
-    if os.path.exists(out+fname+'compressed'):
-        shutil.rmtree(out+fname+'compressed')
-    os.mkdir(out+fname+'compressed')
-    out = out+fname+'compressed\\' if platform.system() == "Windows" else out+fname+'compressed/'
-    
-    # Define device ---------
-    if torch.cuda.is_available():
-        device = torch.device('cuda')
-        accelerator = 'gpu'
-        n_devices = torch.cuda.device_count()
-        if n_devices == 1:
-            print('Device name:', torch.cuda.get_device_name(device))
-        else:
-            print(f'Available devices: {n_devices:>02d}')
-            for i in range(n_devices):
-                print(torch.cuda.get_device_name(i))
-    else:
-        device = torch.device('cpu')
-        accelerator = 'cpu'
-        n_devices = None
-        print('CUDA is not available')
-
-    # Read trajectory -------
-    traj_ = read_traj(traj_=traj, top_=top, stride=stride, memmap=memmap)
-    traj_dl = DataLoader(traj_, batch_size=batchSize, shuffle=True, drop_last=True, num_workers=4)
-    print('_'*70+'\n')
-
-    # Train model -----------
-    model.epoch_losses = list(np.loadtxt(model.loss_path, usecols=1))
-    checkpoint_callback = ModelCheckpoint(
-        dirpath=out,
-        filename=f'{fname}checkpoint',
-        save_top_k=1  # Save the best checkpoint
-        )
-    
-    trainer = pl.Trainer(
-        max_epochs=epochs,
-        accelerator=accelerator,
-        devices=n_devices,
-        callbacks=[checkpoint_callback]
-        )
-    
-    trainer.fit(model, traj_dl, ckpt_path=checkpoint)
 
 # -------------------------------------------- 
 #            D E C O M P R E S S                  
