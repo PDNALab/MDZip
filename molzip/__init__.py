d968500f13865e0fe3251277745d744fb1c37775
nami-rangana <nami.rangana@gmail.com>
2025-07-31 14:50:42 -0400

    remove old files

--- molzip/__init__.py
+++
@@ -1,9 +0,0 @@
-from .molzip import compress, decompress
-
-__version__ = "0.1.0"
-
-__all__ = ["compress", "decompress"]
-
-"""
-molzip: A package for compressing and decompressing molecular data. 
-"""
